5f282010657a5852ef5d76240eeb565585c1187c
frozaken <marcumail@gmail.com>
2024-03-13 15:38:46 +0100

    todo emails

--- setup.py
+++ setup.py
@@ -3,13 +3,5 @@ import pkg_resources
 
 
 setup(
-    name='hyperiax',
-    description='',
     packages=find_packages(),
-    classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.9',
 )
