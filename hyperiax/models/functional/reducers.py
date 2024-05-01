90e0eee05f469d440e1f37688088e07f24b15bb0
MichaelSev <michaelbs@hotmail.dk>
2024-05-01 19:46:00 +0200

    docstrings
    Updated docstring for models

--- hyperiax/models/functional/reducers.py
+++ hyperiax/models/functional/reducers.py
@@ -3,8 +3,7 @@
 def sum_fuse_children(axis=-1):
     """returns a function that sums an axis of keyed values prefixed with `child_`
 
-    Args:
-        axis (int, optional): the axis to sum over. Defaults to -1.
+    :param axis:  axis (int, optional): the axis to sum over. Defaults to -1.
     """
     def _fuse(**kwargs):
         return {k[6:]:v.sum(axis=axis) for k,v in kwargs.items() if k.startswith('child_')}
--- hyperiax/models/lambdamodels.py
+++ hyperiax/models/lambdamodels.py
@@ -7,15 +7,26 @@ class UpLambda(UpModel):
     Requires an up and fuse function.
     """
     def __init__(self, up_fn, fuse_fn) -> None:
+        """Lambda model that only contains an up interface
+
+        :param up_fn: param intputs from node to fuse_fn
+        :param fuse_fn: function to fuse or do calculations form up parameters
+        """
         super().__init__()
 
         self.up_fn = up_fn
         self.fuse_fn = fuse_fn
 
     def up(self, *args, **kwargs):
+        """ Up function to define values to fuse function
+        :return: input arguments to fuse function
+        """
         return self.up_fn(*args, **kwargs)
     
     def fuse(self, *args, **kwargs):
+        """ Fuse function to define calculations from up parameters
+        :return: calculated values from up parameters to parent node
+        """
         return self.fuse_fn(*args, **kwargs)
     
 class UpDownLambda(UpDownModel):
