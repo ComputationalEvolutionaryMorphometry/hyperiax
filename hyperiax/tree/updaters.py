309aa86d61f9da2b1dc1d63999f5d4ef91a49a5a
Rickyang2233 <gefan_yang@163.com>
2024-05-23 15:12:42 +0200

    Docstrings for tree modules

--- hyperiax/tree/updaters.py
+++ hyperiax/tree/updaters.py
@@ -8,6 +8,15 @@ from typing import Callable
 from functools import partial
 
 def update_noise_inplace(update_f: Callable[[TreeNode,float],float], tree : HypTree, key=False, save_noise=False) -> HypTree:
+    """
+    Update the noise in each node of the tree, inplace.
+
+    :param update_f: the way to update the noise
+    :param tree: The tree to update the noise in
+    :param key: The key to generate the noise with, defaults to jax.random.PRNGKey(0)
+    :param save_noise: Whether to save the old noise, defaults to False
+    :return: The tree with updated noise
+    """
     for node in tree.iter_bfs():
         subkey, key = split(key)
         if save_noise:
