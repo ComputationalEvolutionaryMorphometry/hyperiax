0f10e8c3b474980b9729b366f301be45dfba76a8
MichaelSev <51700673+MichaelSev@users.noreply.github.com>
2025-02-04 08:55:37 +0100

    Update tree.py

--- hyperiax/tree/tree.py
+++ hyperiax/tree/tree.py
@@ -1,219 +1,128 @@
-from __future__ import annotations
-from collections import deque
-from dataclasses import dataclass, field
-from jax import numpy as jnp
-from typing import Iterator, List
-from functools import partial
-import time
-import jax
+from typing import Type
 
-@dataclass
-class TopologyNode:
-    parent : TopologyNode = None
-    children : List[TopologyNode] = field(default_factory=list)
-    _backref : HypTree = None
-    name : str = None
-    def __getitem__(self, arg):
-        if not self._backref:
-            raise ValueError("This node is not part of a tree")
-        return self._backref.data[arg][self.id]
-    
-class HypTree:
-    def __init__(self, topology, precompute_child_gathers = False) -> None:
-        self.topology_root = topology
+from .tree import TopologyNode, HypTree
 
-        nodes = 0
-        parents = [0]
-        for i,node in enumerate(self.iter_topology_bfs()):
-            nodes += 1
-            node.id = i
-            node._backref = self
-            if node.parent: 
-                parents.append(node.parent.id)
-            #child_counts.append(len(node.children)) 
+def symmetric_topology(height : int, degree : int, fake_root : bool = False) -> TopologyNode:
+    """ Generate tree of given height and degree
 
-        
-        
-        
-        self.size = nodes
-        self.data = {}
-        self.masks = {}
+    A tree of height zero contains just the root;
+    a tree of height one contains the root and one level of leaves below it,
+    and so forth.
 
-
-        self.levels = list(self._calculate_levels())
-        self.node_depths = jnp.concatenate([i*jnp.ones((lend-lstart, 1), dtype=int) for i,(lstart,lend) in enumerate(self.levels)])
-
-        self.parents = jnp.array(parents)
-        self.child_counts = jax.ops.segment_sum(jnp.ones((self.size,), dtype=int), self.parents, num_segments=self.size, indices_are_sorted=True)
-        self.child_counts = self.child_counts.at[0].add(-1)
-        self.is_leaf = ~self.child_counts.astype(bool)
-        self.is_root = jnp.zeros((self.size,), dtype=bool)
-        self.is_root = self.is_root.at[0].set(True)
-        self.is_inner = ~self.is_leaf & ~self.is_root
-
-        pbuckets = jnp.copy(self.parents)
-        pbuckets_ref = []
-        for lmin,lmax in self.levels:
-            pvals = pbuckets[lmin:lmax]
-            pbuckets_refi, pbucket = jnp.unique(pvals, return_inverse=True)
-            pbuckets = pbuckets.at[lmin:lmax].set(pbucket)
-            pbuckets_ref.append(pbuckets_refi)
-        self.pbuckets = pbuckets
-        self.pbuckets_ref = pbuckets_ref
-
-        #self.child_counts = jnp.array(child_counts)
-        self.depth = len(self.levels)-1
-        self.leaf_limits = self.levels[-1]
-
-        self.coloring = jnp.zeros_like(self.parents, dtype=bool)
-        for lmin, lmax in self.levels[::2]:
-            self.coloring = self.coloring.at[lmin:lmax].set(True)
-
-        if precompute_child_gathers:
-            uniq = jnp.unique(self.child_counts)
-            if len(uniq) == 2:
-                null, base = uniq
-                assert null == 0
-                self.base = base
-                #self.gather_child_idx = jnp.zeros((self.size, self.base), dtype=int)
-                children = []
-                for i,node in enumerate(self.iter_topology_bfs()):
-                    if not node.children: children.append(int(base)*[0])
-                    else: children.append([c.id for c in node.children])
-                self.gather_child_idx = jnp.array(children) 
-            else:
-                raise ValueError("Only trees with the same number of children are supported")
-            # 2 unique values implies that the tree has a "nice" structure
-
-
-
-    def _calculate_levels(self):
-        queue = deque()
-        buffer_queue = deque([self.topology_root])
-        while queue or buffer_queue:
-            if not queue: # if queue is empty, flush the buffer and yield a level
-                queue = buffer_queue
-                yield (buffer_queue[0].id, buffer_queue[-1].id+1) # to not pass the reference
-                buffer_queue = deque()
-
-            if children := queue.popleft().children:
-                buffer_queue.extend(children)
-
-
-    def __len__(self) -> int:
-        return self.size
-
-    def add_property(self, name, shape = (), dtype = None, initializer = None, key = None):
-        self.data[name] = jnp.empty((self.size, *shape), dtype=dtype)
-        self.masks[name] = jnp.zeros((self.size,), dtype=bool)
-
-    def __repr__(self):
-        """
-        Return a string representation of the tree
-        """
-        return f"HypTree(size={self.size}, levels={len(self.levels)}, leaves={jnp.sum(self.is_leaf)}, inner nodes={jnp.sum(self.is_inner)})"
-
-    def __str__(self):
-        return self.__repr__()
-
-
-    ################################
-    # travelsal methods for the tree
-    ################################
-    # dfs
-    def iter_topology_dfs(self) -> Iterator[TopologyNode]:
-        """
-        Iterate over all of the nodes in a depth-first manner.
-
-        """
-        stack = deque([self.topology_root])
-        
-        while stack:
-            current = stack.pop()
-            if current.children:
-                stack.extend(reversed(current.children))
-            yield current
-       
-    def iter_topology_leaves_dfs(self) -> Iterator[TopologyNode]:
-        """
-        Iterate over all of the leaves in the tree, in a depth-first manner.
-        """
-        stack = deque([self.topology_root])
-
-        while stack:
-            current = stack.pop()
-            if current.children:
-                stack.extend(reversed(current.children))
-            else:
-                yield current
-
-    # bfs
-    def iter_topology_bfs(self) -> Iterator[TopologyNode]:
-        """
-        Iterate over all of the nodes in a breadth first manner
-
-        """
-        queue = deque([self.topology_root])
-
-        while queue:
-            current = queue.popleft()
-            if current.children:
-                queue.extend(current.children)
-            yield current
-    
-    def iter_topology_leaves_bfs(self) -> Iterator[TopologyNode]:
-        """
-        Iterate over all of the leaves in the tree, in a breadth-first manner.
-        """
-        queue = deque([self.topology_root])
-
-        while queue:
-            current = queue.popleft()
-            if current.children:
-                queue.extend(current.children)
-            else:
-                yield current
-    
-
-    def iter_topology_levels(self) -> Iterator[List[TopologyNode]]:
-        """
-        Iterate over each level in the tree
-
-        """
-        queue = deque()
-        buffer_queue = deque([self.topology_root])
-        while queue or buffer_queue:
-            if not queue: # if queue is empty, flush the buffer and yield a level
-                queue = buffer_queue
-                yield list(buffer_queue) # to not pass the reference
-                buffer_queue = deque()
-
-            if children := queue.popleft().children:
-                buffer_queue.extend(children)   
-
-
-class FastBiTree(HypTree):
+    :param height: The height of the tree
+    :param degree: The degree of each node in the tree
+    :param new_node: The node used to construct the tree, defaults to TreeNode
+    :param fake_root: The fake root node, defaults to None
+    :raises ValueError: If height is negative
+    :return: The constructed tree
     """
-        Mostly for simulation purposes, creates a bifurcating tree
-        without the topology, making instantiation fast
+    assert height >= 0, f'Height expected to be nonngeative, received {height=}.'
+
+    def _builder(h: int, degree: int, parent):
+        node = TopologyNode(); node.parent = parent; node.children = []
+        if h > 1:
+            node.children = [_builder(h - 1, degree, node) for _ in range(degree)]
+        return node
+
+    return _builder(height + 1, degree, None)
+
+def asymmetric_topology(h: int)  -> TopologyNode:
+    """ 
+    Generate an asymmetric binary tree of given height.
+    A tree of height zero contains just the root;
+    a tree of height one contains the root and one level of leaves below it, and so forth.
+
+    :param h: The height of the tree
+    :raises ValueError: If height is negative
+    :return: The constructed tree
     """
-    def __init__(self, depth) -> None:
-        self.size = (2<<depth)-1
-        self.data = {}
-        self.masks = {}
+    if h < 0:
+        raise ValueError(f'Height shall be nonnegative integer, received {h=}.')
+    elif h == 0:
+        return ChildList([TreeNode()])
 
-        self.levels = [(0,1)]+[((2<<i)-1, (2<<(i+1))-1) for i in range(depth)]
-        self.parents = jnp.concatenate([jnp.array([0]),jnp.arange(self.size-1)//2])
-        self.depth = len(self.levels)-1
-        self.leaves = self.levels[-1]
+    # Fake root 
+    root = TopologyNode(); root.parent = None; root.children = []
+    root.children = [TopologyNode(children= [],parent=root), TopologyNode(children=[],parent=root)]
+   
+    node = root.children[0]
 
-## would love to get this work but https://github.com/google/jax/issues/4269 makes 
-## me believe this is probably a non JAX approach
-class TreeField:
-    def __init__(self, size, shape) -> None:
-        raise NotImplementedError()
-        self.shape = shape
-        self.mask = jnp.zeros((size,), dtype=bool)
-        self.data = jnp.empty((size, *shape))
-    def __repr__(self) -> str:
-        return str(self.data)
+    for _ in range(h - 1):
+        node.children=  [TopologyNode(children=[],parent=node), TopologyNode(children=[],parent=node)]
+
+        node = node.children[0]
+
+    return root 
+
+def read_topology(newick_str: str, return_topology=False, precompute_child_gathers=True) -> HypTree:
+    """ 
+    Generate a tree from a Newick string recursively.
+
+    :param newick_str: newick string representation
+    :return: The constructed tree
+    """
+    import re
+
+    iter_tokens = re.finditer(r"([A-Za-z0-9_-]+)?(?:\s*:\s*([\d.]+))?([,();])", newick_str)
+    edge_lengths = []
+
+    def recursive_parse_newick(parent=None):
+        name, length, char = next(iter_tokens).groups()
+        node = TopologyNode(name=name if name else "", parent=parent, children=[])
+        if length:
+            edge_lengths.append(length)
+
+        if char == "(":
+            while char in "(,":
+                child, char = recursive_parse_newick(parent=node)
+                if child is not None:
+                    node.children.append(child)
+   
+            name, length, char = next(iter_tokens).groups()
+            if name:  # Update node name if one exists
+                node.name = name
+            if length:
+                edge_lengths.append(length)
+        return node, char
+
+
+    root, _ = recursive_parse_newick()
+
+    if return_topology:
+        return root
+    else: 
+
+        tree = HypTree(root,precompute_child_gathers)
+        if root.children:
+            tree.add_property('edge_length', (1,), dtype=float)
+
+            for temp_length,node in zip(edge_lengths,list(reversed(list(tree.iter_topology_dfs())))):
+                tree.data["edge_length"] = tree.data["edge_length"].at[node.id].set(float(temp_length))
+                node.data = {"edge_length": float(temp_length)} if temp_length else {} # Should consider to be removed, but is used for plotting atm.
+
+
+        return tree
+
+def write_topology(tree:HypTree) -> str:
+    """
+    Convert a tree to a Newick string representation.
+
+    :param tree: The tree to convert
+    :return: Newick string representation of the tree
+    """
+    # Check if edge_length exists, otherwise fill it with ones
+    if "edge_length" not in tree.data:
+        tree.add_property('edge_length', shape=(1,))
+        tree.data['edge_length'] =  tree.data['edge_length'].at[:].set(1)
+
+    def recursive_to_newick(node) -> str:
+        edge_length = tree.data["edge_length"].at[node.id].get().item()
+
+        if not node.children:
+            return f"{node.name}:{edge_length:.4f}" if node.name else f":{edge_length:.4f}"
+        
+        children_str = ",".join(recursive_to_newick(child) for child in node.children)
+        node_str = f"({children_str}){node.name}:{edge_length:.4f}" if node.name else f"({children_str}):{edge_length:.4f}"
+        return node_str
+
+    return recursive_to_newick(tree.topology_root) + ";"
