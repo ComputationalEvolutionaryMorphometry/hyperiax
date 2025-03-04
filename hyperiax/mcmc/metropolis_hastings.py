plotting updates: avoid plotting temp node names (xx_); use dots instead of circles for shapes; z-order. mcmc updates: fix in variance parameter; minor plot changes; more flexible accept/reject scheme

hyperiax/mcmc/metropolis_hastings.py
@@ -41,10 +41,9 @@ def metropolis_hastings(log_target, proposal, data, init, num_samples, burn_in=0
         # Compute acceptance probability
         log_likelihood_proposed = log_target(data,proposed_state)
         alpha = jnp.minimum(1.0, jnp.exp(log_likelihood_proposed-log_likelihood_current+log_correction))
-        #print(alpha,log_likelihood_proposed,log_likelihood_current,proposed_state[0].values(),current_state[0].values())
 
         # Decide whether to accept the proposed state
-        if jax.random.uniform(subkey) < alpha:
+        if jax.random.uniform(subkey)<alpha or (hasattr(proposal,'accept_next_state') and proposal.accept_next_state):
             current_state = proposed_state
             log_likelihood_current = log_likelihood_proposed
             accepted_count += 1  # Increment count if proposal is accepted

hyperiax/mcmc/plotting.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # trace plots for MCMC
-def trace_plots(samples,true_params=None):
+def trace_plots(samples,true_params=None,rewrite_names=None):
     """ Trace plots 
     
     :param samples: A list of ParameterStore objects.
@@ -19,13 +19,14 @@ def trace_plots(samples,true_params=None):
 
     # Plot traces
     for ax, (i, param) in zip(axes.ravel(), enumerate(samples[0].values().keys())):
+        name = param if rewrite_names is None else rewrite_names[param]
         ax.plot(np.array([sample[param].value for sample in samples]))
-        ax.set_title(f"Trace for {param}")
+        ax.set_title(f"Trace for {name}")
         ax.set_xlabel('Iteration')
         # Add mean line
         mean_val = np.mean([sample[param].value for sample in samples])
         ax.axhline(y=mean_val,color='r',linestyle='-')
-        ax.set_ylabel(param)
+        ax.set_ylabel(name)
         ax.grid(True)
         if true_params is not None:
             ax.axhline(y=true_params[param],color='g',linestyle='--')

hyperiax/plotting/tree_plots.py
@@ -100,11 +100,12 @@ def plot_node(self,parent,ax,inc_names):
     """Plot a single node and its children"""
     ax.plot(self.data["x_temp"][parent.id],self.data["y_temp"][parent.id], 'ko')  # Plot the current node
 
-    if inc_names:
+    if inc_names and parent.name is not None and not parent.name.startswith('xx_'):
         ax.text(self.data["x_temp"][parent.id].item(),self.data["y_temp"][parent.id].item(),  parent.name+"  ", fontdict=None,rotation="vertical",va="top",ha="center")
-        1+1
+ 
+
     for child in parent.children:
-        if inc_names:
+        if inc_names and child.name is not None and not child.name.startswith('xx_'):
             ax.text(self.data["x_temp"][child.id].item(),self.data["y_temp"][child.id].item(), child.name+"  ", fontdict=None,rotation="vertical",va="top",ha="center")
         
         
@@ -280,7 +281,7 @@ def draw_box(ax, x, y, dis):
     
 
 
-def plot_node_shape(self, parent, ax, inc_names, dis,property,level):
+def plot_node_shape(self, parent, ax, inc_names, dis, property, level, zorder_traj=1, zorder_box=2):
     from matplotlib import pyplot as plt
 
     x0 = self.data["x_temp"][parent.id]
@@ -296,24 +297,24 @@ def plot_node_shape(self, parent, ax, inc_names, dis,property,level):
         plot_trajectory = len(self.data[property][child.id].shape) > 1
 
         if not plot_trajectory:
-            # Draw horizontal and vertical lines
+            # Draw horizontal and vertical lines with lower zorder
             if len(parent.children) > 1:
                 if x<x0-.5*dis or x>x0+.5*dis:
-                    ax.plot([x,x0-dis if x<x0 else x0+dis], [y0,y0],'k-')
-                ax.plot([x,x],[y0 if x<x0-.5*dis or x>x0+.5*dis else y0-dis,y+dis],'k')      
+                    ax.plot([x,x0-dis if x<x0 else x0+dis], [y0,y0],'k-', zorder=zorder_traj)
+                ax.plot([x,x],[y0 if x<x0-.5*dis or x>x0+.5*dis else y0-dis,y+dis],'k', zorder=zorder_traj)      
             else:
-                ax.plot([x,x],[y0-dis,y+dis],'k')
+                ax.plot([x,x],[y0-dis,y+dis],'k', zorder=zorder_traj)
 
-            # Draw box for shape
-            draw_box(ax, x, y, dis)
-
-            # Plot points
+            # Plot points with lower zorder
             points = scale_points(self.data[property][child.id].reshape((-1,2)),[(x-dis,y-dis),(x+dis,y+dis)])
-            ax.scatter(points[:,0], points[:,1],color='r',marker='.')
+            ax.scatter(points[:,0], points[:,1],color='r',marker='.', zorder=zorder_traj)
             
+            # Draw box with higher zorder
+            draw_box(ax, x, y, dis, zorder=zorder_box)
+
             self.data['temp_plotted_point'] = self.data['temp_plotted_point'].at[child.id].set(points)
         else:
-            # Plot points
+            # Plot trajectory with lower zorder
             points = scale_points(self.data[property][child.id].reshape((-1,2)),[(x-dis,y-dis),(x+dis,y+dis)]).reshape((self.data[property][child.id].shape[0],-1,2))
             if 'temp_plotted_point' in self.data.keys():
                 # linearly interpolate
@@ -324,8 +325,8 @@ def plot_node_shape(self, parent, ax, inc_names, dis,property,level):
                 interpolated_array = np.linspace(parent_last_point-child_first_point, np.zeros_like(child_last_point), num_points)
                 points = points+interpolated_array
             for i in range(points.shape[1]):
-                ax.plot(points[:,i,0], points[:,i,1],color=cmap(level))
-                ax.plot(*points[-1,i], 'ro')
+                ax.plot(points[:,i,0], points[:,i,1],color=cmap(level), zorder=zorder_traj)
+                ax.plot(*points[-1,i], 'r.', zorder=zorder_box)
             self.data['temp_plotted_point'] = self.data['temp_plotted_point'].at[child.id].set(points[-1])
 
         # Include text